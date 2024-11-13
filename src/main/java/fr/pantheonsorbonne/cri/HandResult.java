be071b3caae51a8e264b095d1c667b150a716dd7
curdette <cybiae@gmail.com>
2024-11-13 12:54:07 +0000

    Changement handresult (problem de class non résolue)

--- src/main/java/fr/pantheonsorbonne/cri/HandResult.java
+++ src/main/java/fr/pantheonsorbonne/cri/HandResult.java
@@ -39,7 +39,18 @@ public class HandResult {
         }
 
     }
-
+    public static Card[] trierCards(Card[] cards){
+        for(int i =0;i<cards.length; i++){
+            for(int j =i+1; j<cards.length; j++){
+                if(cards[i].getValue().ordinal()<cards[j].getValue().ordinal()){
+                    Card aux = cards[i];
+                    cards[i] = cards[j];
+                    cards[j]=aux;
+                }
+            }
+        }
+        return cards;
+    }
     public int getNumRepetition() {
         return maxCardCount;
 
@@ -47,5 +58,38 @@ public class HandResult {
 
     public Value getHighCard() {
         return cardValue;
+    }
+    public static MainPoker determinerMain(Card[] cards) {
+        int compteurSuite=0;
+        int compteurQuiteFlush =0;
+        cards = HandResult.trierCards(cards);
+        HandResult test = new HandResult(cards);
+        Value highestCard = test.getHighCard();
+        int numrepetition = test.getNumRepetition();
+        if (numrepetition == 2) {
+            return new Paire(cards);
+        } else if (numrepetition==3) {
+            return new Brelan(cards);
+        }
+        else if (numrepetition==4) {
+            return new Carre(cards);
+        }
+        for(int i =0; i<cards.length-1; i++){
+            if(cards[i++].getValue().ordinal()==cards[i].getValue().ordinal()-1){
+                compteurSuite ++;
+                if(cards[i++].getColor().equals(cards[i].getColor())){
+                    compteurSuite=0;
+                    compteurQuiteFlush ++;
+                }
+            } 
+        }
+         if (compteurSuite==4){
+            return new Suite(cards);
+        }
+        else if (compteurQuiteFlush==4){
+            return new QuinteFlush(cards);
+        }
+
+        // Ajoutez les autres conditions pour les autres combinaisons
     }
 }
